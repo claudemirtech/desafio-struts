9ed7c7dbb389c6f3e11eaae4e938a000ad700f6a
claudemir.pinto <claudemir.pinto@softplan.com.br>
2021-10-09 12:19:03 -0300

    Inicia o projeto

---
+++ src/main/java/br/com/atomicweb/action/IndexAction.java
@@ -0,0 +1,18 @@
+package br.com.atomicweb.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class IndexAction extends ActionSupport {
+
+	/**
+	 * https://struts.apache.org/getting-started/index.html
+	 */
+	private static final long serialVersionUID = -7644996148668016677L;
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return "sucess";
+	}
+
+}
